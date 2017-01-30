aa392c246ab3b4824c016dfd8adf4722f73185f1
Rernokk <ssmith37@uccs.edu>
2017-01-30 16:13:02 -0700

    start
    asdf

---
+++ Unity/IndieProject/Assets/Scripts/Dev_Testing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dev_Testing : MonoBehaviour {
+    //Spawning Trigger
+    public GameObject obj;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(obj);
+        }
+	}
+}
---
+++ Unity/IndieProject/Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Equipment {
+
+    void Start()
+    {
+        if (objectName == "")
+        {
+            objectName = Random.Range(0, 100).ToString();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            print(objectName);
+        }
+    }
+}
