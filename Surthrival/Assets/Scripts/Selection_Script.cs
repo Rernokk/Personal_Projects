cab2569b45b11d6cefd4d7640ded99c7f41df75a
Rernokk <ssmith37@uccs.edu>
2017-05-03 15:09:55 -0600

    Beginning Structure of Surthrival

---
+++ Surthrival/Assets/Scripts/Selection_Script.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection_Script : MonoBehaviour {
+    public GameObject curr;
+    [SerializeField]
+    List<GameObject> Structures;
+    [SerializeField]
+    GameObject myObj;
+	// Use this for initialization
+	void Start () {
+        //curr = Structures[0];
+        myObj = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            curr = null;
+            Destroy(myObj);
+            curr = Structures[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            curr = null;
+            Destroy(myObj);
+            curr = Structures[1];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            curr = null;
+            Destroy(myObj);
+            curr = Structures[2];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            curr = null;
+            Destroy(myObj);
+            curr = Structures[3];
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            curr = null;
+            Destroy(myObj);
+            myObj = null;
+        }
+
+        if (curr != null && myObj == null)
+        {
+            myObj = Instantiate(curr, Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, curr.transform.localScale.y / 2, 0), Quaternion.identity);
+        }
+    }
+}
---
+++ Surthrival/Assets/Scripts/Zoning_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoning_Script : MonoBehaviour {
+    Material rend;
+	// Use this for initialization
+	void Start () {
+        rend = transform.GetComponent<MeshRenderer>().material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit) && hit.transform.tag == "Ground"){
+            transform.position = hit.point + new Vector3(0, transform.localScale.y/2, 0);
+            rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, 1f);
+        } else
+        {
+            rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, 0);
+        }
+	}
+}
